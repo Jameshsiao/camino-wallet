df81e5b3a5ff33342fb15d8638de95bee2e00ba3
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-08-31 11:24:25 +0100

    add guide model

---
+++ src/components/modals/GuideModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <Modal
+            ref="modal"
+            :title="$t('kyc_process.title')"
+            class="modal_main"
+            @beforeClose="beforeClose"
+        ></Modal>
+    </div>
+</template>
+<script lang="ts">
+import 'reflect-metadata'
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+import Modal from '@/components/modals/Modal.vue'
+
+@Component({
+    components: {
+        Modal,
+    },
+})
+export default class GuideModal extends Vue {
+    $refs!: {
+        modal: Modal
+    }
+
+    async open() {
+        this.$refs.modal.open()
+    }
+
+    async close() {}
+
+    beforeClose() {}
+}
+</script>
+<style scoped lang="scss">
+@use '../../styles/main';
+</style>

179b4a2b358e891b1c093bf8cac0f0d7a6aa86cd
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-09-01 09:23:04 +0100

    popup the guide model when wallet created

---
+++ src/components/modals/CustomModel.vue
@@ -0,0 +1,31 @@
+<template>
+    <transition name="fade">
+        <!-- <div class="modal_main" v-if="isActive">
+            <div class="modal_bg" @click="bgclick" :icy="icy"></div>
+            <div class="modal_body">
+                <div class="modal_topbar">
+                    <h4 class="modal_title">{{ title }}</h4>
+                    <button class="modalClose" @click="close" v-if="can_close">
+                        <fa icon="times"></fa>
+                    </button>
+                </div>
+                <slot></slot>
+            </div>
+        </div> -->
+    </transition>
+</template>
+<script lang="ts">
+import 'reflect-metadata'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+@Component
+export default class CustomModel extends Vue {
+    @Prop({ default: 'Model Title' }) title!: string
+    @Prop({ default: 'Model SubTitle' }) sub_title!: string
+    @Prop({ default: true }) can_close!: string
+    @Prop({ default: false }) icy!: boolean
+
+    isActive: boolean = false
+
+    public open() {}
+}
+</script>
--- src/components/modals/GuideModal.vue
+++ src/components/modals/GuideModal.vue
@@ -2,36 +2,109 @@
     <div>
         <Modal
             ref="modal"
-            :title="$t('kyc_process.title')"
+            title="Simplify Creating Wallet"
             class="modal_main"
             @beforeClose="beforeClose"
-        ></Modal>
+        >
+            <div v-if="kycStatus">
+                <div>checked</div>
+                <div>{{ kycStatus }}</div>
+            </div>
+            <div v-else @click="openKyc">go to check</div>
+            <button
+                v-if="walletType === 'mnemonic'"
+                :tooltip="$t('top.hover2')"
+                @click="viewPrintModal"
+                class="print_but"
+            >
+                <v-icon>mdi-water-outline</v-icon>
+            </button>
+        </Modal>
+        <KycModal ref="kyc_modal"></KycModal>
+        <paper-wallet
+            ref="print_modal"
+            v-if="walletType === 'mnemonic'"
+            :wallet="activeWallet"
+        ></paper-wallet>
     </div>
 </template>
 <script lang="ts">
 import 'reflect-metadata'
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
 import Modal from '@/components/modals/Modal.vue'
-
+import KycModal from './KycModal.vue'
+import PaperWallet from './PaperWallet/PaperWallet.vue'
+import { WalletType, WalletNameType } from '@/js/wallets/types'
 @Component({
     components: {
         Modal,
+        KycModal,
+        PaperWallet,
     },
 })
 export default class GuideModal extends Vue {
     $refs!: {
         modal: Modal
+        kyc_modal: KycModal
+        print_modal: PaperWallet
     }
 
     async open() {
         this.$refs.modal.open()
     }
-
-    async close() {}
-
+    viewPrintModal() {
+        this.$refs.print_modal.open()
+    }
+    async close() {
+        this.$refs.modal.close()
+    }
+    get walletType(): WalletNameType {
+        let wallet = this.activeWallet
+        if (!wallet) return 'mnemonic'
+        return wallet.type
+    }
+    get activeWallet(): WalletType | null {
+        return this.$store.state.activeWallet
+    }
+    get kycStatus(): boolean {
+        return this.$store.getters['Accounts/kycStatus']
+    }
+    openKyc() {
+        this.$refs.kyc_modal.open()
+        console.log('asdfkjhdsfh')
+    }
+    mounted() {
+        console.log(this.kycStatus)
+        console.log(this.$store.state.activeWallet.getCurrentAddressPlatform())
+    }
     beforeClose() {}
 }
 </script>
 <style scoped lang="scss">
 @use '../../styles/main';
+.modal_main::v-deep {
+    .modal_body {
+        width: 70%;
+        max-width: 750px;
+        height: min-content !important;
+        /* min-height: 450px !important; */
+        border-radius: var(--border-radius-sm) !important;
+        overflow: auto;
+        min-height: 200px;
+        @include main.mobile-device {
+            max-height: 90vh;
+            max-width: none;
+            width: 80%;
+            min-height: fit-content;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+    .modal_bg {
+        width: 100vw !important;
+        position: fixed;
+    }
+}
 </style>

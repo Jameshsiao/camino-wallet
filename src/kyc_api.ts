e7ef3396e4963f7a24c3b082f3590e863c4d8fea
aeddaqqa <achrafeddaqqaq@gmail.com>
2023-01-09 17:28:10 +0100

    update how the token generated for kyc

--- src/kyc_api.ts
+++ src/kyc_api.ts
@@ -1,11 +1,22 @@
 import axios, { AxiosInstance } from 'axios'
-
+import { keccak_256 } from 'js-sha3'
+let elliptic = require('elliptic')
+let ec = new elliptic.ec('secp256k1')
 export interface AccessToken {
     token: string
     userId: string
 }
 
-const BASE_URL = 'https://kyc-poc-be.camino.foundation/api/v1'
+/* Convert a byte to string */
+function byte2hexStr(byte: number) {
+    var hexByteMap = '0123456789ABCDEF'
+    var str = ''
+    str += hexByteMap.charAt(byte >> 4)
+    str += hexByteMap.charAt(byte & 0x0f)
+    return str
+}
+
+const BASE_URL = 'https://kyc-poc-be-playground.camino.foundation/api/v1'
 
 const kyc_api: AxiosInstance = axios.create({
     baseURL: BASE_URL,
@@ -15,12 +26,29 @@ const kyc_api: AxiosInstance = axios.create({
     },
 })
 
-async function generateToken(address: string, publicKeyByte: string): Promise<AccessToken> {
-    let url = `/generate_token`
+async function generateToken(privateKey: string): Promise<AccessToken> {
+    const token: string = (await kyc_api.get('/token')).data
+    console.log(token)
+    let msgHash = keccak_256(token)
+    let signature = ec.sign(msgHash, privateKey)
+    let r = signature.r
+    let s = signature.s
+    let id = signature.recoveryParam
+
+    let rHex = r.toString('hex')
+    while (rHex.length < 64) {
+        rHex = '0' + rHex
+    }
+    let sHex = s.toString('hex')
+    while (sHex.length < 64) {
+        sHex = '0' + sHex
+    }
+    let idHex = byte2hexStr(id)
+    let signHex = '0x' + rHex + sHex + idHex
     try {
-        let res = await kyc_api.post(url, {
-            external_user_id: address,
-            public_key: publicKeyByte,
+        let res = await kyc_api.post('/generate_ss_token', {
+            token: token,
+            signature: signHex,
         })
         return res.data
     } catch (e) {
